c1fe6d768421f42f6ef99840880268ccae3fff51
Arvind Bhakuni <arvindbhakuni75@gmail.com>
2023-02-08 22:52:04 +0530

    final commit

--- src/container/ProductComponent.js
+++ src/container/ProductComponent.js
@@ -8,24 +8,26 @@ export default function ProductComponent() {
     const { id, image, title, price, category } = product;
 
     return (
-      
-        <div className="col-md-3 my-3" key={i} >
-          <div className="card" >
-            <Link to={`/product/${id}`} style={{color: "black"}}>
-              <img src={image} className="card-img-top p-4" alt="..." style={{ height: "300px" }} />
-              <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <h5 className="card-title">$ {price}</h5>
-                <p className="card-text text-uppercase">{category}</p>
-                <p href="#" className="btn btn-primary">
-                  Add To Cart
-                </p>
-              </div>
+      <div className="col-md-3 my-3" key={i}>
+        <div className="card">
+          <img
+            src={image}
+            className="card-img-top p-4"
+            alt="..."
+            style={{ height: "300px" }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">$ {price}</h5>
+            <p className="card-text text-uppercase">{category}</p>
+            <Link to={`/product/${id}`} style={{ color: "black" }}>
+              <p href="#" className="btn btn-primary">
+                View Details
+              </p>
             </Link>
           </div>
         </div>
-        
-      
+      </div>
     );
   });
 
